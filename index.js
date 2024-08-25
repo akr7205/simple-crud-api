2143b5ce40f61ec073a0abf7b5be4aaad0d5e7d4
Arun Rout <you@example.com>
2024-08-25 16:56:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+app.use("/api/products", productRoute);
+
+//update a product
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    console.log("product updated", product);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+//delete product
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    console.log("product deleted", product);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://akr7205:admin7205@cluster0.hhysy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
---
+++ routes/product.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/product.model.js");
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product.controller.js");
+
+router.get("/", getProducts);
+router.get("/:id", getProduct);
+
+router.post("/", createProduct);
+//update product
+router.put("/:id", updateProduct);
+//delete product
+router.delete("/:id", deleteProduct);
+module.exports = router;
